ac3421864c592400f5476fdc2426c6bd851ce577
payne-wu <wuzhipeng1289690157@gmail.com>
2022-09-12 14:42:34 +0800

    format

--- converter.js
+++ converter.js
@@ -7,9 +7,7 @@ const traverse = require('@babel/traverse').default;
 
 const chaos_file = '';
 let chaos_code = fs.readFileSync(chaos_file, {encoding: 'utf-8'});
-let ast_code = parse(chaos_code, {
-    sourceType: "module",
-});
+let ast_code = parse(chaos_code, {sourceType: "module"});
 
 // process rules visitor
 const visitor = {}
@@ -22,11 +20,7 @@ visitors_container.forEach(rule => {
 
 // generate clean js file
 const {code} = generator(ast_code, {
-    minified: false,
-    comments: false,
-    retainLines: false,
-    compact: false,
-    jsescOption: {},
+    minified: false, comments: false, retainLines: false, compact: false, jsescOption: {},
 })
 // output clean js code
 fs.writeFileSync(decodeFile, code, {encoding: "utf-8"});
