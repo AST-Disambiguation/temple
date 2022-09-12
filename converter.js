b63a522e72f1f2d8c893d37915bf389c57db31f0
payne-wu <wuzhipeng1289690157@gmail.com>
2022-09-12 14:44:45 +0800

    format

--- converter.js
+++ converter.js
@@ -1,13 +1,13 @@
 "use strict";
 const fs = require('fs');
-const {parse} = require('@babel/parser');
+const { parse } = require('@babel/parser');
 const types = require('@babel/types');
 const generator = require('@babel/generator').default;
 const traverse = require('@babel/traverse').default;
 
 const chaos_file = '';
-let chaos_code = fs.readFileSync(chaos_file, {encoding: 'utf-8'});
-let ast_code = parse(chaos_code, {sourceType: "module"});
+let chaos_code = fs.readFileSync(chaos_file, { encoding: 'utf-8' });
+let ast_code = parse(chaos_code, { sourceType: "module" });
 
 // process rules visitor
 const visitor = {}
@@ -15,12 +15,12 @@ const visitor = {}
 const visitors_container = [visitor];
 
 visitors_container.forEach(rule => {
-    traverse(ast_code, rule, {name: "OE"})
+    traverse(ast_code, rule, { name: "OE" })
 });
 
 // generate clean js file
-const {code} = generator(ast_code, {
+const { code } = generator(ast_code, {
     minified: false, comments: false, retainLines: false, compact: false, jsescOption: {},
 })
 // output clean js code
-fs.writeFileSync(decodeFile, code, {encoding: "utf-8"});
+fs.writeFileSync(decodeFile, code, { encoding: "utf-8" });
